e6f73caca0607f7ac161f3cda43a4514670f1f65
Ubuntu <ubuntu@ip-172-31-5-22.ec2.internal>
2021-11-24 22:33:09 +0000

    Created the basicMath.cpp and areaPerRect.cpp files. In the basicMath.cpp file I answered questions using math operations and in the areaPerRect.cpp file I found the area and perimeter of a rectangle

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Ina Tolo All rights reserved.
+// Created by: Ina Tolo
+// Created on: Nov. 24, 2021
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 9cm X 4cm.
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 9cm and width 4cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 9 * 4 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (9 + 4) << " cm\n";
+}
